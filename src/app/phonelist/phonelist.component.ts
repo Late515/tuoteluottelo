a8712bd70b94da02971a4a709446a2f79ea17194
Late515 <s2201201@edu.bc.fi>
2023-03-07 10:39:18 +0200

    "osa2 bootstrap + phone.service.ts + phonelist"

---
+++ src/app/phonelist/phonelist.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-phonelist',
+  templateUrl: './phonelist.component.html',
+  styleUrls: ['./phonelist.component.css'],
+})
+export class PhonelistComponent {}
