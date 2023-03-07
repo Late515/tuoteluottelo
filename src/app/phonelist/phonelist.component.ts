9889bb3572919957ea8175f0c2429871336f048c
Late515 <s2201201@edu.bc.fi>
2023-03-07 13:59:18 +0200

    "osa3 nyt saadaan tiedot puhelimista"

--- src/app/phonelist/phonelist.component.ts
+++ src/app/phonelist/phonelist.component.ts
@@ -1,8 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import phoneData from '/Users/s2201201/Documents/portfolio/angularin-perusteet/angular-projektit/datan-hakeminen-json-tiedostosta/src/assets/phones/phones.json';
+interface Phone {
+  age: Number;
+  id: String;
+  imageUrl: String;
+  name: String;
+  snippet: String;
+}
 
 @Component({
   selector: 'app-phonelist',
   templateUrl: './phonelist.component.html',
   styleUrls: ['./phonelist.component.css'],
 })
-export class PhonelistComponent {}
+export class PhonelistComponent implements OnInit {
+  constructor() {}
+
+  phones: Phone[] = phoneData;
+
+  ngOnInit(): void {}
+}
